b756d6c45d77dbd418fa2b843076b3677864a6fa
Daniel Hambleton <daniel.hambleton@meshconsultants.ca>
2017-01-23 11:57:19 -0500

    input panel is back

--- Components/Input_Panel.cpp
+++ Components/Input_Panel.cpp
@@ -9,11 +9,19 @@ using namespace Urho3D;
 
 String Input_Panel::iconTexture = "Textures/Icons/Input_Panel.png";
 
+Input_Panel::Input_Panel(Urho3D::Context* context) :
+	IoComponentBase(context, 1, 1),
+	editable_(true)
+{ 
+	SubscribeToEvent("OnSolveGraph", URHO3D_HANDLER(Input_Panel, HandleGraphSolve));
+}
+
 void Input_Panel::SolveInstance(
 	const Vector<Variant>& inSolveInstance,
 	Vector<Variant>& outSolveInstance
 	)
 {
+	
 	outSolveInstance[0] = inSolveInstance[0];
 }
 
@@ -21,165 +29,54 @@ void Input_Panel::SolveInstance(
 
 void Input_Panel::HandleCustomInterface(UIElement* customElement)
 {
-	
-	uiRoot = customElement;
-
-	//LineEdit* l = (LineEdit*)customElement->GetChild("PropertyEdit", true);
-	LineEdit* l = customElement->CreateChild<LineEdit>("PropertyEdit");
-	l->SetStyle("LineEdit");
-	if (l)
-	{
-		SubscribeToEvent(l, E_TEXTFINISHED, URHO3D_HANDLER(Input_Panel, HandleLineEditCommit));
-		lines.Push(l);
-	}
-
-	addLine = (Button*)customElement->GetChild("AddTextLine", true);
-	if (addLine)
-	{
-		SubscribeToEvent(addLine, E_PRESSED, URHO3D_HANDLER(Input_Panel, HandleNewLine));
-	}
-
-	deleteLine = (Button*)customElement->GetChild("DeleteTextLine", true);
-	if (deleteLine)
-	{
-		SubscribeToEvent(deleteLine, E_PRESSED, URHO3D_HANDLER(Input_Panel, HandleDeleteLine));
-	}
-
-	//create text line edits from input slot data
-	IoDataTree* tree = inputSlots_[0]->GetIoDataTreePtr();
-	Vector<int> path = tree->Begin();
-	//only iterate through one branch
-	int numItems = tree->GetNumItemsAtBranch(path, DataAccess::ITEM);
-	for (int i = 0; i < numItems; i++)
-	{
-		Variant var;
-		tree->GetNextItem(var, DataAccess::ITEM);
-
-		if (i == 0 && lines.Size() > 0)
-		{
-			l = lines.At(0);
-			l->SetText(var.ToString());
-		}
-		else
-		{
-			l = CreateNewLine();
-			l->SetText(var.ToString());
-		}
-	}
-
-
-	UIElement* controls = customElement->CreateChild<UIElement>("ControlGroup");
-	controls->SetMinHeight(30);
-	controls->SetLayoutMode(LM_HORIZONTAL);
-	controls->SetLayoutSpacing(2);
+	textArea_ = customElement->CreateChild<MultiLineEdit>("TextArea");
+	textArea_->SetStyleAuto();
+	textArea_->SetHeight(100);
+	textArea_->GetTextElement()->SetFontSize(9);
+	SubscribeToEvent(textArea_, E_TEXTFINISHED, URHO3D_HANDLER(Input_Panel, HandleLineEditCommit));
 }
 
-String Input_Panel::GetNodeStyle()
+void Input_Panel::HandleGraphSolve(Urho3D::StringHash eventType, Urho3D::VariantMap& eventData)
 {
-	return "TextLineNode";
+	if (inputSlots_[0]->HasNoData())
+	{
+		editable_ = true;
+		textArea_->SetEditable(true);
+	}
+	else
+	{
+		//try to set panel content
+		IoDataTree* dt = inputSlots_[0]->GetIoDataTreePtr();
+		String dtText = dt->ToString(false);
+
+		if (textArea_.NotNull())
+		{
+			textArea_->SetText(dtText);
+			textArea_->SetEditable(false);
+		}
+
+		editable_ = false;
+	}
 }
 
 void Input_Panel::HandleLineEditCommit(StringHash eventType, VariantMap& eventData)
 {
 	using namespace TextFinished;
 
-	LineEdit* l = (LineEdit*)eventData[P_ELEMENT].GetVoidPtr();
-	if (l)
-	{
-		
-		Vector<int> path;
-		path.Push(0);
-		IoDataTree tree(GetContext());
-		
-		for (int i = 0; i < lines.Size(); i++)
-		{
-			String val = lines[i]->GetText();
-
-			VariantType type = VAR_STRING;
-
-			//try to find type by splitting with comma
-			Vector<String> parts = val.Split(',');
-			if (parts.Size() > 1)
-			{
-				type = Variant::GetTypeFromName(parts[1].Trimmed());
-			}
-
-			Variant var(type, val);
-			
-			tree.Add(path, var);
-		}
-		
-		
-		InputHardSet(0, tree);
-
-		GetSubsystem<IoGraph>()->QuickTopoSolveGraph();
-	}
-}
-
-LineEdit* Input_Panel::CreateNewLine()
-{
-	LineEdit* l = uiRoot->CreateChild<LineEdit>();
-	l->SetStyle("LineEdit");
-	l->SetPosition(5, lines.Size() * 32 + 5);
-	l->SetSize(uiRoot->GetSize().x_ - 10, 30);
-
-	SubscribeToEvent(l, E_TEXTFINISHED, URHO3D_HANDLER(Input_Panel, HandleLineEditCommit));
-
-	lines.Push(l);
-
-	//resize ui root
-	uiRoot->SetSize(uiRoot->GetSize().x_, lines.Size() * 32 + 10 + 40);
-
-	//move adders
-	if (addLine && deleteLine)
-	{
-		addLine->SetPosition(addLine->GetPosition().x_, uiRoot->GetSize().y_ - 35);
-		deleteLine->SetPosition(deleteLine->GetPosition().x_, uiRoot->GetSize().y_ - 35);
-	}
-
-	return l;
-
-}
-
-void Input_Panel::HandleNewLine(StringHash eventType, VariantMap& eventData)
-{
-	if (!uiRoot)
+	if (textArea_.Null() || !editable_)
 		return;
 
-	CreateNewLine();
-
-}
-
-void Input_Panel::HandleDeleteLine(StringHash eventType, VariantMap& eventData)
-{
-	if (!uiRoot)
-		return;
-
-	if (lines.Size() <= 1)
-		return;
-
-	LineEdit* l = lines[lines.Size() - 1];
-	l->Remove();
-	lines.Remove(l);
-
-	//resize ui root
-	uiRoot->SetSize(uiRoot->GetSize().x_, lines.Size() * 32 + 10 + 40);
-
-	//move adders
-	if (addLine && deleteLine)
-	{
-		addLine->SetPosition(addLine->GetPosition().x_, uiRoot->GetSize().y_ - 35);
-		deleteLine->SetPosition(deleteLine->GetPosition().x_, uiRoot->GetSize().y_ - 35);
-	}
-
-	//reset the input
+	//create a tree and data path
 	Vector<int> path;
 	path.Push(0);
 	IoDataTree tree(GetContext());
 
+	//split text in to multiple lines
+	StringVector lines = textArea_->GetText().Split('\n');
 	for (int i = 0; i < lines.Size(); i++)
 	{
-		String val = lines[i]->GetText();
+
+		String val = lines[i];
 
 		VariantType type = VAR_STRING;
 
@@ -195,10 +92,12 @@ void Input_Panel::HandleDeleteLine(StringHash eventType, VariantMap& eventData)
 		tree.Add(path, var);
 	}
 
-
+	//set input
 	InputHardSet(0, tree);
 
+	//call solve
 	GetSubsystem<IoGraph>()->QuickTopoSolveGraph();
-
 }
+
+
 
--- Components/Input_Panel.h
+++ Components/Input_Panel.h
@@ -2,6 +2,8 @@
 
 #include <Urho3D/UI/Button.h>
 #include <Urho3D/UI/LineEdit.h>
+#include <Urho3D/UI/MultiLineEdit.h>
+
 #include "IoComponentBase.h"
 
 class URHO3D_API Input_Panel : public IoComponentBase {
@@ -9,7 +11,8 @@ class URHO3D_API Input_Panel : public IoComponentBase {
 	URHO3D_OBJECT(Input_Panel, IoComponentBase)
 
 public:
-	Input_Panel(Urho3D::Context* context) : IoComponentBase(context, 1, 1) { };
+	Input_Panel(Urho3D::Context* context);
+
 public:
 
 	void SolveInstance(
@@ -19,16 +22,12 @@ public:
 
 	static Urho3D::String iconTexture;
 
-	virtual void HandleCustomInterface(Urho3D::UIElement* customElement);
-	virtual Urho3D::String GetNodeStyle();
+	Urho3D::SharedPtr<Urho3D::MultiLineEdit> textArea_;
 
-	Urho3D::UIElement* uiRoot = NULL;
-	Urho3D::Button* addLine = NULL;
-	Urho3D::Button* deleteLine = NULL;
-	Urho3D::Vector<Urho3D::LineEdit*> lines;
+	virtual void HandleCustomInterface(Urho3D::UIElement* customElement);
+	bool editable_;
 
 	void HandleLineEditCommit(Urho3D::StringHash eventType, Urho3D::VariantMap& eventData);
-	void HandleNewLine(Urho3D::StringHash eventType, Urho3D::VariantMap& eventData);
-	void HandleDeleteLine(Urho3D::StringHash eventType, Urho3D::VariantMap& eventData);
-	Urho3D::LineEdit* CreateNewLine();
+	void HandleGraphSolve(Urho3D::StringHash eventType, Urho3D::VariantMap& eventData);
+
 };
