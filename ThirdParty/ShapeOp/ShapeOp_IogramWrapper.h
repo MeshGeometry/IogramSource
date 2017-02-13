34916a52e1b4861f3fed15b5aeae5f8c21b48746
Chris Cappadocia <christopher.cappadocia@meshconsultants.ca>
2017-02-13 10:09:33 -0500

    delete or repurpose code in ShapeOp_IogramWrapper

--- ThirdParty/ShapeOp/ShapeOp_IogramWrapper.h
+++ ThirdParty/ShapeOp/ShapeOp_IogramWrapper.h
@@ -8,47 +8,14 @@
 ////////////////////
 // ShapeOpConstraint
 
-bool IsValidShapeOpConstraint(
-	const Urho3D::String& constraint_type,
-	const Urho3D::VariantVector& id_list,
-	int nb_points,
-	float weight
-);
-
-Urho3D::Variant ShapeOpConstraint_Make(
-	const Urho3D::String& constraint_type,
-	const Urho3D::VariantVector& id_list,
-	int nb_points,
-	float weight
-);
-
 bool ShapeOpConstraint_Verify(const Urho3D::Variant& constraint);
 
 Urho3D::String ShapeOpConstraint_constraintType(const Urho3D::Variant& constraint);
-std::vector<int> ShapeOpConstraint_ids(const Urho3D::Variant& constraint);
-int ShapeOpConstraint_nb_ids(const Urho3D::Variant& constraint);
+
 double ShapeOpConstraint_weight(const Urho3D::Variant& constraint);
 
-std::vector<double> ShapeOp_TriMeshToPoints(const Urho3D::Variant& tri_mesh);
-
-Urho3D::VariantVector ShapeOp_PointsToVertexList(const std::vector<double>& pts);
-
-//////////////
-// VertexForce
-
-Urho3D::Variant ShapeOpVertexForce_Make(
-	const Urho3D::Vector3& force_on_vertex,
-	int vertex_id
-);
-
-bool ShapeOpVertexForce_Verify(const Urho3D::Variant& vertex_force);
-
-int ShapeOpVertexForce_id(const Urho3D::Variant& vertex_force);
-std::vector<double> ShapeOpVertexForce_force(const Urho3D::Variant& vertex_force);
-
-////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-// New approach
+////////////////
+// ShapeOpVertex
 
 Urho3D::Variant ShapeOpVertex_Make(
 	const Urho3D::Vector3& v
