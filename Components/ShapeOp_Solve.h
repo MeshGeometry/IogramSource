115aa9f6d88da44b3dda92566922169c1c88a671
Chris Cappadocia <christopher.cappadocia@meshconsultants.ca>
2017-02-15 14:51:54 -0500

    ShapeOp persistent solver, works with Restart input slot

--- Components/ShapeOp_Solve.h
+++ Components/ShapeOp_Solve.h
@@ -2,6 +2,18 @@
 
 #include "IoComponentBase.h"
 
+#include <vector>
+
+#include "ShapeOp_API.h"
+
+// ShapeOp's solver natively operates on raw points not meshes.
+// This struct helps track meshes across a ShapeOp simulation.
+struct MeshTrackingData {
+	Urho3D::Variant mesh;
+	std::vector<int> raw_indices;
+	std::vector<int> processed_indices;
+};
+
 class URHO3D_API ShapeOp_Solve : public IoComponentBase {
 	URHO3D_OBJECT(ShapeOp_Solve, IoComponentBase)
 public:
@@ -18,4 +30,11 @@ public:
 	void DeleteOutputSlot(int index) = delete;
 
 	static Urho3D::String iconTexture;
+
+	// state parameters
+	ShapeOpSolver* op = NULL;
+	int m_iterations = 10;
+	int m_nb_points = -1;
+	std::vector<MeshTrackingData> m_tracked_meshes;
+	Urho3D::Vector<int> m_new_indices;
 };
