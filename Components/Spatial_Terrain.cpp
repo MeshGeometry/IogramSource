7e77f05f73c20f2966d4cc7093a4d703bd3b6f37
Daniel Hambleton <daniel@meshconsultants.ca>
2017-02-22 10:01:22 -0500

    minor changes to terrain

--- Components/Spatial_Terrain.cpp
+++ Components/Spatial_Terrain.cpp
@@ -26,8 +26,18 @@ Spatial_Terrain::Spatial_Terrain(Urho3D::Context* context) : IoComponentBase(con
 		"I",
 		"Image File",
 		VAR_STRING,
-		ITEM
+		ITEM,
+		"Textures/HeightMap.png"
 	);
+
+	AddInputSlot(
+		"Material",
+		"M",
+		"Material Path",
+		VAR_STRING,
+		ITEM,
+		"Materials/BasicWeb.xml"
+		);
 
 	AddInputSlot(
 		"Transform",
@@ -37,6 +47,15 @@ Spatial_Terrain::Spatial_Terrain(Urho3D::Context* context) : IoComponentBase(con
 		ITEM,
 		Matrix3x4::IDENTITY
 	);
+
+	AddInputSlot(
+		"Spacing",
+		"S",
+		"Grid Spacing",
+		VAR_VECTOR3,
+		ITEM,
+		Vector3(1, 0.2, 1.0)
+		);
 
 	AddOutputSlot(
 		"NodeID",
@@ -93,18 +112,23 @@ void Spatial_Terrain::SolveInstance(
 	}
 
 	// construct transform:
-	Matrix3x4 xform = Geomlib::ConstructTransform(inSolveInstance[0]);
+	Matrix3x4 xform = Geomlib::ConstructTransform(inSolveInstance[2]);
+	String imagePath = inSolveInstance[0].GetString();
+	String matPath = inSolveInstance[1].GetString();
+	Vector3 spacing = inSolveInstance[3].GetVector3();
 
 	Node* tNode = scene->CreateChild("TerrainNode");
-	tNode->SetTransform(xform);
+
 
 	Terrain* terrain = tNode->CreateComponent<Terrain>();
 	terrain->SetPatchSize(64);
-	terrain->SetSpacing(Vector3(2.0f, 0.1f, 2.0f)); // Spacing between vertices and vertical resolution of the height map
-	terrain->SetSmoothing(true);
-	terrain->SetHeightMap(cache->GetResource<Image>("Textures/HeightMap.png"));
-	terrain->SetMaterial(cache->GetResource<Material>("Materials/Terrain.xml"));
+	terrain->SetSpacing(spacing); // Spacing between vertices and vertical resolution of the height map
+	terrain->SetSmoothing(false);
+	terrain->SetHeightMap(cache->GetResource<Image>(imagePath));
+	terrain->SetMaterial(cache->GetResource<Material>(matPath));
 	terrain->SetOccluder(true);
+
+	tNode->SetTransform(xform);
 
 	trackedItems.Push(tNode->GetID());
 	
