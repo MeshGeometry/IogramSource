74245e6ca0e31aceab48495d6fe44f32105efc2a
Elissa Ross <elissa.ross@meshconsultants.ca>
2017-02-22 16:57:41 -0500

    TopologyQueries now fast too

--- Components/Mesh_FaceTopology.h
+++ Components/Mesh_FaceTopology.h
@@ -8,17 +8,20 @@ class URHO3D_API Mesh_FaceTopology : public IoComponentBase {
 public:
     Mesh_FaceTopology(Urho3D::Context* context);
     
-    void SolveInstance(
-                       const Urho3D::Vector<Urho3D::Variant>& inSolveInstance,
-                       Urho3D::Vector<Urho3D::Variant>& outSolveInstance
-                       );
+    //void SolveInstance(
+    //                   const Urho3D::Vector<Urho3D::Variant>& inSolveInstance,
+    //                   Urho3D::Vector<Urho3D::Variant>& outSolveInstance
+    //                   );
     
     void AddInputSlot() = delete;
     void AddOutputSlot() = delete;
     void DeleteInputSlot(int index) = delete;
     void DeleteOutputSlot(int index) = delete;
     
-    
+	int LocalSolve();
+
+	IoDataTree ComputeVerticesOnFace(Urho3D::Variant inMeshWithData, IoDataTree& vertexVectorsTree);
+	IoDataTree ComputeAdjacentFaces(Urho3D::Variant inMeshWithData);
     
     static Urho3D::String iconTexture;
 };
