82d95366e89d1fe7124eb07816f0996efafea230
Daniel Hambleton <daniel@meshconsultants.ca>
2017-07-29 15:01:57 -0400

    fix build scripts on linux

---
+++ Build/gcc/Geometry/CMakeFiles/Geometry.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libGeometry.a"
+)
---
+++ Build/gcc/Player/CMakeFiles/IogramPlayer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IogramPlayer.dir/IogramPlayer.cpp.o"
+  "../bin/IogramPlayer.pdb"
+  "../bin/IogramPlayer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IogramPlayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
