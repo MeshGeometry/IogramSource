9567e3eb0c4afb0c4fa53eb21ac87e7f5c7f3abb
Daniel Hambleton <danie@meshconsultants.ca>
2017-07-29 14:36:28 -0400

    minor fix

--- FullBuild_GCC.sh
+++ FullBuild_GCC.sh
@@ -1,5 +1,5 @@
-cmake Urho3D/cmake_generic.sh Build/gcc -DURHO3D_OPENGL=1 -DURHO3D_64BIT=1 -DURHO3D_LUA=0 -DURHO3D_SAMPLES=0 -DURHO3D_C++11=1
-cd Urho3D/Build/gcc && make -j4
+cmake ./Urho3D/cmake_generic.sh ./Urho3D/Build/gcc -DURHO3D_OPENGL=1 -DURHO3D_64BIT=1 -DURHO3D_LUA=0 -DURHO3D_SAMPLES=0 -DURHO3D_C++11=1
+cd ./Urho3D/Build/gcc && make -j4
 cd ../..
 cmake -BBuild/gcc -H. -DURHO3D_HOME="./Urho3D/Build/gcc" -DURHO3D_OPENGL=1 -DURHO3D_64BIT=1 -DURHO3D_LUA=0 -DURHO3D_C++11=1
 cd Build/gcc && make -j4
